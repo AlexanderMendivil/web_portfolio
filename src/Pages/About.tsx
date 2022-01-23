3627074b3e2e00ea122997dbdb130410b527ac6a
Alexander Mendivil <alexanderima@outlook.es>
2022-01-22 18:20:14 -0700

    Se agregó grid

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 
+import "./About.css";
+
 export const About = () => {
     return(
-    <div>
-        <h1>About</h1>
+    <div className="about_container">
+        <div>
+            <h1 className="titulo">About <span className="color">me.</span></h1>
+            <hr />
+            <text></text>
+        </div>
+        
+        <div className="robot">
+            <div>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, sapiente sunt magni veniam illo odio neque itaque ducimus officiis commodi facilis dolor aspernatur? Illum alias atque, sunt obcaecati a cumque repudiandae excepturi nesciunt distinctio nostrum itaque, facilis recusandae in mollitia est temporibus quasi id? Harum laboriosam debitis aut natus quae?
+            </div>
+        </div>
+
     </div>
     );
 };
