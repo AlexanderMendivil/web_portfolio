9eab0a129d311083bd922932cf61efc11e815a5e
Alexander Mendivil <alexanderima@outlook.es>
2022-01-22 18:24:04 -0700

    Se agregaro la estructura de sidebar

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import "./Sidebar.css"
 const Sidebar = () => {
-
+    
     return(
-        <div>
+        <div className="sidebar">
+            <div className="image">
+
+            </div>
         <nav className="navbar">
-            <Link to ="About">About</Link>
-            <Link to ="Skills">My skills</Link>
-            <Link to ="Hobbies">Hobbies</Link>
-            <Link to ="Contact">Contact</Link>
+            <div className="pages">
+                <Link className="link" to ="About">About</Link>
+                <Link className="link" to ="Skills">My skills</Link>
+                <Link className="link" to ="Hobbies">Hobbies</Link>
+                <Link className="link" to ="Contact">Contact</Link>
+            </div>
             <hr />
+            
+            <div className="socialMedia">
+                <a href="https://github.com/AlexanderMendivil"><GitHubIcon className="icons"/></a>
+                <a href="https://www.facebook.com/ivanzerro36/"><FacebookIcon className="icons"/></a>
+                <a href="https://www.linkedin.com/in/ivan-alexander-mendivil-araujo-103479213/"><LinkedInIcon className="icons"/></a>
+            </div>
         </nav>
         </div>
     );
